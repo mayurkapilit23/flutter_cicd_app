098224d3510d120b2ce71878b92fe0456ae90d6a
mayurpkapilit23 <mayurp@kapilit.com>
2026-07-14 14:44:05 +0530

    code format

--- lib/main.dart
+++ lib/main.dart
@@ -20,25 +20,15 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
-
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
 
-
-
-
-
-
         title: Text("Flutter Github actions app"),
       ),
-      body: Center(
-        child:  const Text('Flutter Github actions')
-      )
+      body: Center(child: const Text('Flutter Github actions')),
     );
   }
 }
